7c141fcc40f778b14e5ea4d28dd4685e052fce8d
Maria Murgasova <maria.murgasova04@imperial.ac.uk>
2009-03-23 17:02:02 +0000

    Matching mean and variance

--- packages/segmentation/src/irtkIntensityMatching.cc
+++ packages/segmentation/src/irtkIntensityMatching.cc
@@ -185,5 +185,144 @@ void irtkIntensityMatching::WeightedPCA(double *x1, double *y1, double *weights,
 
 }
 
+void irtkIntensityMatching::WeightedLeastSqaures(double *x1, double *y1, double *weights, int no)
+{
+  cerr<<"Start weighted least square regression"<<endl;
+
+  irtkGreyImage hist(101,101,1), trans(101,101,1);
+  double xxx,yyy;
+  int i;
+
+  for (i=0; i<no; i++) {
+    xxx=100*(x1[i]-_min)/(_max-_min);
+    yyy=100*(y1[i]-_ymin)/(_ymax-_ymin);
+    if ((xxx>=0)&&(xxx<=100)&&(yyy>=0)&&(yyy<=100))hist.Put(round(xxx),round(yyy),0,hist.Get(round(xxx),round(yyy),0)+1);
+  }
+  _hist=hist;
+  hist.Write("hist.nii.gz");
+
+  //expectation values and covarinaces
+  double x=0,y=0,xy=0,x2=0,y2=0;
+  double ex,ey,ex2,exy,ey2;
+  double varx, vary, covxy;
+  double k,l,w;
+  double sum=0;
+
+  for (i=0; i<no; i++) {
+    w=weights[i];
+    sum +=w;
+
+    x += x1[i]*w;
+    y += y1[i]*w;
+    xy += x1[i]*y1[i]*w;
+    x2 += x1[i]*x1[i]*w;
+    y2 += y1[i]*y1[i]*w;
+  }
+
+  ex  = x/sum;
+  ey  = y/sum;
+  ex2 = x2/sum;
+  exy = xy/sum;
+  ey2 = y2/sum;
+
+  varx  = ex2-ex*ex;
+  vary  = ey2-ey*ey;
+  covxy = exy-ex*ey;
+
+  //k = (varx-vary)/(2*covxy);
+  //l = k+sqrt(1+k*k);
+
+  _m = covxy/varx; // 1/l;
+  _b = ey-_m*ex; // /l;
+
+  cerr<<"y = "<<_m<<" * x + "<<_b<<endl;
+
+  double origx;
+  for (i=0; i<=100; i++) {
+    origx=_min+(_max-_min)*(i/100.0);
+    yyy=100*(Lin(origx)-_ymin)/(_ymax-_ymin);
+    if ((yyy>=0)&&(yyy<=100))trans.Put(i,round(yyy),0,1);
+  }
+  trans.Write("trans.nii.gz");
+  _trans=trans;
+
+}
+void irtkIntensityMatching::MatchMeanAndVariance(double *x1, double *y1, double *weights, int no)
+{
+  cerr<<"Start matching"<<endl;
+
+  irtkGreyImage hist(101,101,1), trans(101,101,1);
+  double xxx,yyy;
+  int i;
+
+  for (i=0; i<no; i++) {
+    xxx=100*(x1[i]-_min)/(_max-_min);
+    yyy=100*(y1[i]-_ymin)/(_ymax-_ymin);
+    if ((xxx>=0)&&(xxx<=100)&&(yyy>=0)&&(yyy<=100))hist.Put(round(xxx),round(yyy),0,hist.Get(round(xxx),round(yyy),0)+1);
+  }
+  _hist=hist;
+  hist.Write("hist.nii.gz");
+
+  //expectation values and covarinaces
+  double x=0,y=0,xy=0,x2=0,y2=0;
+  double ex,ey,ex2,exy,ey2;
+  double varx, vary, covxy;
+  double k,l,w;
+  double sum=0;
+
+  for (i=0; i<no; i++) {
+    w=weights[i];
+    sum +=w;
+
+    x += x1[i]*w;
+    y += y1[i]*w;
+    xy += x1[i]*y1[i]*w;
+    x2 += x1[i]*x1[i]*w;
+    y2 += y1[i]*y1[i]*w;
+  }
+
+  ex  = x/sum;
+  ey  = y/sum;
+  ex2 = x2/sum;
+  exy = xy/sum;
+  ey2 = y2/sum;
+
+  varx  = ex2-ex*ex;
+  vary  = ey2-ey*ey;
+  covxy = exy-ex*ey;
+
+  double eps = 0.001;
+
+  if (covxy>eps)
+  {
+	  _m = sqrt(vary/varx);
+	  _b = ey-_m*ex;
+  }
+
+  if (covxy<eps)
+  {
+	  _m = -sqrt(vary/varx);
+	  _b = ey-_m*ex;
+  }
+
+  if (abs(covxy)<= eps)
+  {
+	  _m = 1;
+	  _b=ey-_m*ex;
+  }
+
+  cerr<<"y = "<<_m<<" * x + "<<_b<<endl;
+
+  double origx;
+  for (i=0; i<=100; i++) {
+    origx=_min+(_max-_min)*(i/100.0);
+    yyy=100*(Lin(origx)-_ymin)/(_ymax-_ymin);
+    if ((yyy>=0)&&(yyy<=100))trans.Put(i,round(yyy),0,1);
+  }
+  trans.Write("trans.nii.gz");
+  _trans=trans;
+
+}
+
 
 
