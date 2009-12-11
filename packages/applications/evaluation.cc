4c95166cbe7645848814abedf6a15448021a4f3d
Daniel Rueckert <d.rueckert@imperial.ac.uk>
2009-12-11 13:57:14 +0000

    Modified version to take multiple dofs, real valued images, a mask and to set default bin sizes.

--- packages/applications/evaluation.cc
+++ packages/applications/evaluation.cc
@@ -19,7 +19,11 @@
 #include <irtkTransformation.h>
 
 // Default filenames
-char *source_name = NULL, *target_name, *dof_name  = NULL;
+char *source_name = NULL, *target_name = NULL, *mask_name = NULL;
+char **dof_name  = NULL;
+
+#define MAX_DOFS 10
+#define DEFAULT_BINS 255
 
 // Default number of bins
 int nbins_x = 0, nbins_y = 0;
@@ -28,9 +32,9 @@ void usage()
 {
   cerr << "Usage: evaluation [target] [source] <options>\n" << endl;
   cerr << "where <options> is one or more of the following: \n" << endl;
-  cerr << "<-dofin file>      Transformation" << endl;
-  cerr << "<-nbins_x no>      Number of bins for target" << endl;
-  cerr << "<-nbins_y no>      Number of bins for source" << endl;
+  cerr << "<-dofin file>      Transformation (Multiple dofs possible, processed in order given)." << endl;
+  cerr << "<-nbins_x no>      Number of bins for target (Default smaller of dynamic range or 255)" << endl;
+  cerr << "<-nbins_y no>      Number of bins for source (Default as above)" << endl;
   cerr << "<-Rx1 pixel>       Region of interest" << endl;
   cerr << "<-Ry1 pixel>       Region of interest" << endl;
   cerr << "<-Rz1 pixel>       Region of interest" << endl;
@@ -38,6 +42,7 @@ void usage()
   cerr << "<-Ry2 pixel>       Region of interest" << endl;
   cerr << "<-Rz2 pixel>       Region of interest" << endl;
   cerr << "<-Tp  value>       Padding value in target" << endl;
+  cerr << "<-mask file>       Binary mask to define ROI" << endl;
   cerr << "<-linear>          Linear interpolation" << endl;
   cerr << "<-bspline>         B-spline interpolation" << endl;
   cerr << "<-cspline>         Cubic spline interpolation" << endl;
@@ -47,14 +52,15 @@ void usage()
 
 int main(int argc, char **argv)
 {
-  irtkTransformation *transformation = NULL;
+  irtkTransformation **transformation = NULL;
   irtkInterpolateImageFunction *interpolator = NULL;
-  irtkGreyPixel target_min, source_min, target_max, source_max;
-  int ok, x, y, z, i1, j1, k1, i2, j2, k2, Tp;
-  double x1, y1, z1, x2, y2, z2;
+  irtkRealPixel target_min, source_min, target_max, source_max;
+  int ok, x, y, z, i1, j1, k1, i2, j2, k2; 
+  double x1, y1, z1, x2, y2, z2, Tp, widthx, widthy, val;
+  int i, noOfDofs;
 
   // Check command line
-  if (argc < 3) {
+  if (argc < 3){
     usage();
   }
 
@@ -68,15 +74,16 @@ int main(int argc, char **argv)
 
   // Read target image
   cout << "Reading target ... "; cout.flush();
-  irtkGreyImage target(target_name);
+  irtkRealImage target(target_name);
   cout << "done" << endl;
   // Read source image
   cout << "Reading source ... "; cout.flush();
-  irtkGreyImage source(source_name);
+  irtkRealImage source(source_name);
   cout << "done" << endl;
 
+  Tp = -1.0 * FLT_MAX;
+
   // Fix ROI
-  Tp = -INT_MAX;
   i1 = 0;
   j1 = 0;
   k1 = 0;
@@ -88,17 +95,23 @@ int main(int argc, char **argv)
   nbins_x = 0;
   nbins_y = 0;
 
-  while (argc > 1) {
+  // Fix number of dofs
+  noOfDofs = 0;
+
+  dof_name = new char*[MAX_DOFS];
+
+  while (argc > 1){
     ok = False;
-    if ((ok == False) && (strcmp(argv[1], "-dofin") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-dofin") == 0)){
       argc--;
       argv++;
-      dof_name = argv[1];
+      dof_name[noOfDofs] = argv[1];
+      noOfDofs++;
       argc--;
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-nbins_x") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-nbins_x") == 0)){
       argc--;
       argv++;
       nbins_x = atoi(argv[1]);
@@ -106,7 +119,7 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-nbins_y") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-nbins_y") == 0)){
       argc--;
       argv++;
       nbins_y = atoi(argv[1]);
@@ -114,15 +127,15 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-Tp") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-Tp") == 0)){
       argc--;
       argv++;
-      Tp = atoi(argv[1]);
+      Tp = atof(argv[1]);
       argc--;
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-Rx1") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-Rx1") == 0)){
       argc--;
       argv++;
       i1 = atoi(argv[1]);
@@ -130,7 +143,7 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-Rx2") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-Rx2") == 0)){
       argc--;
       argv++;
       i2 = atoi(argv[1]);
@@ -138,7 +151,7 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-Ry1") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-Ry1") == 0)){
       argc--;
       argv++;
       j1 = atoi(argv[1]);
@@ -146,7 +159,7 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-Ry2") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-Ry2") == 0)){
       argc--;
       argv++;
       j2 = atoi(argv[1]);
@@ -154,7 +167,7 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-Rz1") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-Rz1") == 0)){
       argc--;
       argv++;
       k1 = atoi(argv[1]);
@@ -162,7 +175,7 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-Rz2") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-Rz2") == 0)){
       argc--;
       argv++;
       k2 = atoi(argv[1]);
@@ -170,58 +183,107 @@ int main(int argc, char **argv)
       argv++;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-linear") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-linear") == 0)){
       argc--;
       argv++;
       interpolator = new irtkLinearInterpolateImageFunction;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-bspline") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-bspline") == 0)){
       argc--;
       argv++;
       interpolator = new irtkBSplineInterpolateImageFunction;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-cspline") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-cspline") == 0)){
       argc--;
       argv++;
       interpolator = new irtkCSplineInterpolateImageFunction;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-sinc") == 0)) {
+    if ((ok == False) && (strcmp(argv[1], "-sinc") == 0)){
       argc--;
       argv++;
       interpolator = new irtkSincInterpolateImageFunction;
       ok = True;
     }
-    if (ok == False) {
+    if ((ok == False) && (strcmp(argv[1], "-mask") == 0)){
+      argc--;
+      argv++;
+      mask_name = argv[1];
+      argc--;
+      argv++;
+      ok = True;
+    }
+    if (ok == False){
       cerr << "Can not parse argument " << argv[1] << endl;
       usage();
     }
   }
+  
+  irtkGreyImage mask;
+  
+  // Set up a mask, 
+  if (mask_name == NULL){
+    mask.Read(target_name);
+
+    irtkGreyPixel *ptr2mask = mask.GetPointerToVoxels();
+    irtkRealPixel *ptr2tgt  = target.GetPointerToVoxels();
+
+    for (i = 0; i < target.GetNumberOfVoxels(); i++){
+      if (*ptr2tgt > Tp)
+        *ptr2mask = 1;
+      else
+        *ptr2mask = 0;
+      
+      ++ptr2tgt;
+      ++ptr2mask;
+    }
+  } else {
+    mask.Read(mask_name);
+    if ((mask.GetX() != target.GetX()) ||
+        (mask.GetY() != target.GetY()) ||
+        (mask.GetZ() != target.GetZ())){
+      cerr << "evaluation2: Target and mask dimensions mismatch. Exiting." << endl;
+      exit(1);
+    }
+  }
+  
+    
+  // if there is a ROI apply to mask also.
 
   // If there is an region of interest, use it
   if ((i1 != 0) || (i2 != target.GetX()) ||
-      (j1 != 0) || (j2 != target.GetY()) ||
-      (k1 != 0) || (k2 != target.GetZ())) {
+      (j1 != 0) || (j2 != target.GetY()) || 
+      (k1 != 0) || (k2 != target.GetZ())){
     target = target.GetRegion(i1, j1, k1, i2, j2, k2);
     source = source.GetRegion(i1, j1, k1, i2, j2, k2);
+    mask   = mask.GetRegion(i1, j1, k1, i2, j2, k2);
   }
 
   // Set min and max of histogram
   target.GetMinMax(&target_min, &target_max);
   source.GetMinMax(&source_min, &source_max);
-  cout << "Min and max of X is " << target_min
-       << " and " << target_max << endl;
-  cout << "Min and max of Y is " << source_min
-       << " and " << source_max << endl;
+  cout << "Min and max of X is " << target_min 
+      << " and " << target_max << endl;
+  cout << "Min and max of Y is " << source_min 
+      << " and " << source_max << endl;
 
   // Calculate number of bins to use
-  if (nbins_x == 0) nbins_x = target_max - target_min + 1;
-  if (nbins_y == 0) nbins_y = source_max - source_min + 1;
+  if (nbins_x == 0){
+    nbins_x = (int) round(target_max - target_min) + 1;
+    if (nbins_x > DEFAULT_BINS)
+      nbins_x = DEFAULT_BINS;
+  }
+
+  if (nbins_y == 0){
+    nbins_y = (int) round(source_max - source_min) + 1;
+    if (nbins_y > DEFAULT_BINS)
+      nbins_y = DEFAULT_BINS;
+  }
 
   // Create default interpolator if necessary
-  if (interpolator == NULL) {
+  if (interpolator == NULL){
     interpolator = new irtkNearestNeighborInterpolateImageFunction;
   }
   interpolator->SetInput(&source);
@@ -232,38 +294,74 @@ int main(int argc, char **argv)
 
   // Create histogram
   irtkHistogram_2D histogram(nbins_x, nbins_y);
-  histogram.PutMin(target_min - 0.5, source_min - 0.5);
-  histogram.PutMax(target_max + 0.5, source_max + 0.5);
 
-  if (dof_name != NULL) {
-    // Read transformation
-    transformation = irtkTransformation::New(dof_name);
+  widthx = (target_max - target_min) / (nbins_x - 1.0);
+  widthy = (source_max - source_min) / (nbins_y - 1.0);
+
+  histogram.PutMin(target_min - 0.5*widthx, source_min - 0.5*widthy);
+  histogram.PutMax(target_max + 0.5*widthx, source_max + 0.5*widthy);
+
+  if (noOfDofs == 0){
+    noOfDofs = 1;
+    transformation = new irtkTransformation*[noOfDofs];
+    transformation[0] = new irtkRigidTransformation;
   } else {
-    transformation = new irtkRigidTransformation;
+    transformation = new irtkTransformation*[noOfDofs];
+    for (i = 0; i < noOfDofs; i++){
+      transformation[i] = irtkTransformation::New(dof_name[i]);
+    }
   }
 
+  target_min = FLT_MAX;
+  source_min = FLT_MAX;
+  target_max = -1.0 * FLT_MAX;
+  source_max = -1.0 * FLT_MAX;
+
   // Fill histogram
-  for (z = 0; z < target.GetZ(); z++) {
-    for (y = 0; y < target.GetY(); y++) {
-      for (x = 0; x < target.GetX(); x++) {
-        if (target(x,y,z) > Tp) {
+  for (z = 0; z < target.GetZ(); z++){
+    for (y = 0; y < target.GetY(); y++){
+      for (x = 0; x < target.GetX(); x++){
+
+        if (mask(x, y, z) > 0){
+          val = target(x, y, z);
+          
+          if (val > target_max)
+            target_max = val;
+          if (val < target_min)
+            target_min = val;
+
           irtkPoint p(x, y, z);
           // Transform point into world coordinates
           target.ImageToWorld(p);
           // Transform point
-          transformation->Transform(p);
+          for (i = 0; i < noOfDofs; i++){
+            transformation[i]->Transform(p);
+          }
           // Transform point into image coordinates
           source.WorldToImage(p);
           if ((p._x > x1) && (p._x < x2) &&
               (p._y > y1) && (p._y < y2) &&
-              (p._z > z1) && (p._z < z2)) {
-            histogram.AddSample(target(x, y, z), round(interpolator->EvaluateInside(p._x, p._y, p._z)));
+              (p._z > z1) && (p._z < z2)){
+
+            val = interpolator->EvaluateInside(p._x, p._y, p._z);
+
+            histogram.AddSample(target(x, y, z), val);
+            if (val >  source_max)
+              source_max = val;
+            if (val < source_min)
+              source_min = val;
+
           }
         }
       }
     }
   }
 
+  cout << "ROI Min and max of X is " << target_min 
+      << " and " << target_max << endl;
+  cout << "ROI Min and max of Y is " << source_min 
+      << " and " << source_max << endl;
+  cout << "Number of bins  X x Y : " << histogram.GetNumberOfBinsX() << " x " << histogram.GetNumberOfBinsY() << endl;
   cout << "Number of Samples: "     << histogram.NumberOfSamples() << endl;
   cout << "Mean of X: "             << histogram.MeanX() << endl;
   cout << "Mean of Y: "             << histogram.MeanY() << endl;
@@ -279,9 +377,8 @@ int main(int argc, char **argv)
   cout << "Norm. Mutual Information: " << histogram.NormalizedMutualInformation() << endl;
   cout << "Correlation ratio C(X|Y): " << histogram.CorrelationRatioXY() << endl;
   cout << "Correlation ratio C(Y|X): " << histogram.CorrelationRatioYX() << endl;
-  if (nbins_x == nbins_y) {
+  if (nbins_x == nbins_y){
     cout << "Label consistency: " << histogram.LabelConsistency() << endl;
     cout << "Kappa statistic: " << histogram.Kappa() << endl;
   }
 }
-
