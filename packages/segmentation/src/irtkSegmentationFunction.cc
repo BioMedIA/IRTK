bcbe95af3834226483725dd7192292a4d1e7b904
Wenzhe Shi <wenzhe.shi07@imperial.ac.uk>
2011-04-28 11:11:22 +0000

    properties

--- packages/segmentation/src/irtkSegmentationFunction.cc
+++ packages/segmentation/src/irtkSegmentationFunction.cc
@@ -1,3 +1,15 @@
+/*=========================================================================
+
+  Library   : Image Registration Toolkit (IRTK)
+  Module    : $Id$
+  Copyright : Imperial College, Department of Computing
+              Visual Information Processing (VIP), 2009 onwards
+  Date      : $Date$
+  Version   : $Revision$
+  Changes   : $Author$
+
+=========================================================================*/
+
 #include "irtkSegmentationFunction.h"
 #ifdef HAS_OPENCV
 /// detect possible presence of the object at threshold == 3
