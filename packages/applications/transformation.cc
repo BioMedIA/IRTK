46eb97b38890eb90d8c88df4c2f639418f0f6a85
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2011-05-16 16:48:49 +0000

    Reversed previous change. Too messy.

--- packages/applications/transformation.cc
+++ packages/applications/transformation.cc
@@ -96,8 +96,6 @@ void usage()
 	cerr << "<-cspline>         Cubic spline interpolation" << endl;
 	cerr << "<-sbased>          Shape based interpolation" << endl;
 	cerr << "<-sinc>            Sinc interpolation" << endl;
-	cerr << endl;
-	cerr << "Revision: $Revision$ " << endl;
 	exit(1);
 }
 
