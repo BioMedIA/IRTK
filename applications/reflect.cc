6e08f0a9635de6b53f1cd3bd44cc9e1980211348
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2011-11-16 14:52:50 +0000

    Enable multiple reflections with more types (e.g. flip x/y). Allow arbitrary image data type.

--- applications/reflect.cc
+++ applications/reflect.cc
@@ -9,22 +9,31 @@
   Changes   : $Author$
 
 =========================================================================*/
-
 #include <irtkImage.h>
+#include <irtkFileToImage.h>
 
 char *input_name = NULL, *output_name = NULL;
 
+char **reflect_list = NULL;
+
 void usage()
 {
-  cerr << "Usage: reflect [in] [out] [-x/-y/-z]" << endl;
+  cerr << "Usage: reflect [in] [out] [reflection_1] <reflection_2> .. <reflection_n>" << endl;
+  cerr << "Where the reflections are chosen from:" << endl;
+  cerr << " " << endl;
+  cerr << "        -x -y -z -xy -xz -yz" << endl;
+  cerr << " " << endl;
+  cerr << "The reflections are processed in the order given." << endl;
+
   exit(1);
 }
 
 int main(int argc, char **argv)
 {
-  irtkGreyImage image;
+  int i, count;
+  irtkBaseImage *image;
 
-  if (argc != 4) {
+  if (argc < 4){
     usage();
   }
 
@@ -35,26 +44,51 @@ int main(int argc, char **argv)
   argc--;
   argv++;
 
-  // Read input
-  image.Read(input_name);
+  count = argc - 1;
+  reflect_list = new char *[count];
+  for (i = 0; i < count; ++i){
+    reflect_list[i] = argv[1];
+    argc--;
+    argv++;
+  }
 
-  if (strcmp("-x", argv[1]) == 0) {
-    image.ReflectX();
-  } else {
-    if (strcmp("-y", argv[1]) == 0) {
-      image.ReflectY();
-    } else {
-      if (strcmp("-z", argv[1]) == 0) {
-        image.ReflectZ();
-      } else {
-        usage();
-      }
+  // Read input
+  irtkFileToImage *reader = irtkFileToImage::New(input_name);
+  image = reader->GetOutput();
+
+  for (i = 0; i < count; ++i){
+    if (strcmp("-x", reflect_list[i]) == 0){
+      image->ReflectX();
+    }
+
+    if (strcmp("-y", reflect_list[i]) == 0){
+     image->ReflectY();
+    }
+
+    if (strcmp("-z", reflect_list[i]) == 0){
+        image->ReflectZ();
+    }
+
+    if (strcmp("-xy", reflect_list[i]) == 0 ||
+        strcmp("-yx", reflect_list[i]) == 0){
+      image->FlipXY();
+    }
+
+    if (strcmp("-xz", reflect_list[i]) == 0 ||
+        strcmp("-zx", reflect_list[i]) == 0){
+      image->FlipXZ();
+    }
+
+    if (strcmp("-yz", reflect_list[i]) == 0 ||
+        strcmp("-zy", reflect_list[i]) == 0){
+      image->FlipYZ();
     }
   }
 
-
   // Write region
-  image.Write(output_name);
+  image->Write(output_name);
+
+  delete [] reflect_list;
 
   return 0;
 }
