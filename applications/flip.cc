558ef9cd26ca5f5b24c8f43f6f9809b574cbc09d
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2011-11-17 18:21:19 +0000

    Clarified usage and added parameter to Flip calls.

--- applications/flip.cc
+++ applications/flip.cc
@@ -16,7 +16,11 @@ char *input_name = NULL, *output_name = NULL;
 
 void usage()
 {
-  cerr << "Usage: flip [in] [out] [-xy/-xz/-xt/-yz/-yz/-zt]" << endl;
+  cerr << " Usage: flip [in] [out] [-xy/-xz/-xt/-yz/-yz/-zt]" << endl;
+  cerr << " " << endl;
+  cerr << " Swap the specified dimensions of the image grid _and_ " << endl;
+  cerr << " swap the corresponding world coordinates of the centre " << endl;
+  cerr << " of the grid (origin information in header)." << endl;
   exit(1);
 }
 
@@ -39,22 +43,22 @@ int main(int argc, char **argv)
   image.Read(input_name);
 
   if (strcmp("-xy", argv[1]) == 0) {
-    image.FlipXY();
+    image.FlipXY(1);
   } else {
     if (strcmp("-xz", argv[1]) == 0) {
-      image.FlipXZ();
+      image.FlipXZ(1);
     } else {
       if (strcmp("-xt", argv[1]) == 0) {
-        image.FlipXT();
+        image.FlipXT(1);
       } else {
         if (strcmp("-yz", argv[1]) == 0) {
-          image.FlipYZ();
+          image.FlipYZ(1);
         } else {
           if (strcmp("-yt", argv[1]) == 0) {
-            image.FlipYT();
+            image.FlipYT(1);
           } else {
             if (strcmp("-zt", argv[1]) == 0) {
-              image.FlipZT();
+              image.FlipZT(1);
             } else {
               usage();
             }
