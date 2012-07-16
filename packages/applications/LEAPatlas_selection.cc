587389fa50a958b752e94c055c08029e215618ad
Robin Wolz <r.wolz@imperial.ac.uk>
2012-07-16 15:29:26 +0000

    Added executables required for LEAP package

---
+++ packages/applications/LEAPatlas_selection.cc
@@ -0,0 +1,198 @@
+
+#include <irtkRegistration.h>
+#include <PairwiseSimilarity.h>
+#include <SpectralClustering.h>
+
+irtkMatrix read_csv(string csvFilename1, string csvFilename2);
+void writeAtlases(string filename, int * atlases, int nrAtlases);
+void writeAtlasNames(string filename, int * atlases, int nrAtlases, vector<string> atlasNames);
+void usage()
+{
+	cerr << "usage LEAPatlas_selection [Atlas Similarities] [Target Similarities] [N selected atlases] [output]" << endl;
+	cerr << "-atlasNames [list of atlas names]. With this option, atlas names are written instead of numbers." << endl;
+
+		
+}
+int main(int argc, char **argv)
+{
+	if(argc < 4){
+		usage();
+		exit(1);
+	}
+
+	string atlassims = argv[1];
+	argc--;
+	argv++;
+	string targetsims = argv[1];
+	argc--;
+	argv++;
+	int nrAtlases = atoi(argv[1]);
+	argc--;
+	argv++;
+	string outname = argv[1];
+	argc--;
+	argv++;
+
+
+	bool ok;
+	bool useAtlasNames = false;
+	vector<string> atlasNames = vector<string>();
+	while (argc > 1){
+		ok = false;
+		if ((ok == false) && (strcmp(argv[1], "-atlasNames") == 0)){
+		  argc--;
+		  argv++;
+
+		  string line;
+		  string imagefilename = argv[1];
+		  argc--;
+		  argv++;
+		  cout << "Using atlas names from " << imagefilename << endl;
+		  ifstream from(imagefilename.c_str());
+		  if (!from) {
+			cerr << "Can't open file "
+			<< endl;
+			exit(1);
+		  }
+		  if (from.is_open())	{
+			while (! from.eof() ) {
+			  getline (from,line);
+			  string fileName = line;
+			  atlasNames.push_back(fileName);
+			}
+			from.close();
+		  }
+		  useAtlasNames = true;
+		  ok = true;
+		}
+		if (ok == false){
+			cerr << "Can not parse argument " << argv[1] << endl;
+			usage();
+		}
+	}
+	cout << "Read similarity matrices" << endl;
+	irtkMatrix w;
+	try{
+		w = read_csv(atlassims, targetsims);
+	}
+	catch (int e){
+		if(e == 1){
+			cerr << "File not found exception" << endl;
+		}
+		return -1;
+	}
+	SpectralClustering le(w.Rows(), 2);
+	cout << "Do manifold embedding" << endl;
+	le.Initialize(w);
+	le.DoSpectralEmbedding();
+	int selected_atlases[nrAtlases];
+	cout << "Select " << nrAtlases << " closest atlases (IDs in [0...nAtlases-1]): " << endl;
+	le.GetNeighbours(w.Rows()-1, nrAtlases, selected_atlases);
+
+	if(useAtlasNames)
+		writeAtlasNames(outname, selected_atlases, nrAtlases, atlasNames);
+	else
+		writeAtlases(outname, selected_atlases, nrAtlases);
+}
+
+irtkMatrix read_csv(string csvFilename1, string csvFilename2){
+
+	ifstream input;
+	input.open(csvFilename1.c_str());
+	string value;
+	int rows = 0;
+	if(! input){
+		cerr << "File " << csvFilename1 << " does not exist" << endl;
+		throw 1;
+	}
+	while(input.good()){
+		getline(input, value, '\n');
+		rows++;
+
+	}
+	rows--;
+//	if(rows != _nrSubjects){
+//		cerr << "Dimensions in .csv file do not agree with nrSubjects" << endl;
+//	}
+	irtkMatrix _w;
+	_w.Initialize(rows+1, rows+1);
+
+	ifstream input2;
+	input2.open(csvFilename1.c_str());
+	int rowctr = 0;
+	int colctr = 0;
+	while(input2.good()){
+		getline(input2, value, '\n');
+		int pos = value.find(",");
+		int posOld = -1;
+		while(colctr < rows && rowctr < rows){
+			string stringdata = value.substr(posOld+1,pos-posOld-1);
+			posOld = pos;
+			pos = value.find(",", posOld+1);
+			double data = atof(stringdata.c_str());
+			_w(rowctr, colctr) = data;
+			colctr++;
+		}
+		colctr = 0;
+		rowctr ++;
+//		cout << rowctr << endl;
+	}
+//	cout << "a" << endl;
+	ifstream input3;
+	input3.open(csvFilename2.c_str());
+	if(! input3){
+			cerr << "File " << csvFilename2 << " does not exist" << endl;
+			throw 1;
+		}
+	rowctr = 0;
+	while(input3.good() && rowctr < rows){
+		getline(input3, value, '\n');
+		double data = atof(value.c_str());
+		_w(rowctr, rows) = data;
+		_w(rows, rowctr) = data;
+		rowctr++;
+//		cout << data << endl;
+	}
+	if(rowctr != rows){
+		cerr << "No of target similarities (" << rowctr << ") does not agree with No of atlas similarities(" << rows << ")"<< endl;
+
+	}
+	_w(rows, rows) = 2;
+
+
+	return _w;
+
+}
+
+void writeAtlasNames(string filename, int * atlases, int nrAtlases, vector<string> atlasNames){
+	ofstream output;
+	cout << "Write atlas names to '" << filename << "'" << endl;
+	output.open(filename.c_str());
+	for(int r = 0; r < nrAtlases; r++){
+		output << atlasNames[*atlases];
+		output << endl;
+		atlases++;
+
+	}
+	output.close();
+}
+
+void writeAtlases(string filename, int * atlases, int nrAtlases){
+	ofstream output;
+	cout << "Write atlas IDs to '" << filename << "'" << endl;
+	output.open(filename.c_str());
+	for(int r = 0; r < nrAtlases; r++){
+
+
+		output << *atlases;
+	//	cout << *atlases << endl;
+		if(r != nrAtlases-1)
+			output << ","; //endl;
+		atlases++;
+
+	}
+
+	output << endl;
+
+	output.close();
+}
---
+++ packages/applications/LEAPsimilarity_2.cc
@@ -0,0 +1,215 @@
+
+#include <irtkRegistration.h>
+#include <PairwiseSimilarity.h>
+
+#ifdef HAS_TBB
+ #include "tbb/task_scheduler_init.h"
+  #include "tbb/parallel_for.h"
+ #include "tbb/blocked_range.h"
+
+
+ using namespace tbb;
+
+
+ class MultiThreadedLeapSimilarity{
+
+ private:
+	 int _nrImages;
+	 int _sizeSet2;
+	 bool _useMask;
+	 irtkGreyImage ** _masks;
+	 int _simType;
+	 int _nrMasks;
+	 double * _results_nmi;
+	// vector<pair<double,int> > * _results;
+	 vector<string> _files;
+	 string _imagedir;
+	 irtkGreyImage * _target;
+
+
+
+
+ public:
+ 	MultiThreadedLeapSimilarity(string imagedir, irtkGreyImage ** masks, double * results_nmi, irtkGreyImage *target, vector<string> files){
+ 		_useMask = true; //always use a mask --> if not read in, it is generated here
+		_simType = 1; //NMI
+		_sizeSet2 = 1; // we only compare one image to all the others
+		_nrMasks = 1; // always use one mask
+		_nrImages = 1; // one image after the other
+
+		_masks = masks;
+		_results_nmi = results_nmi;
+		_files = files;
+		_imagedir = imagedir;
+		_target = target;
+
+
+ 	}
+
+	void operator()(const blocked_range<int> &r) const {
+		for (int i = r.begin(); i!= r.end(); i++) {
+
+
+			irtkGreyImage ** _images;
+			_images = new irtkGreyImage*[2]; // two images: current atlas and target image
+			_images[0] = new irtkGreyImage(); // first image will be the current atlas
+			_images[1] = new irtkGreyImage(); // second image will always be the target
+			*_images[1] = *_target;
+
+
+			cout << "Atlas " << i << ". Image ";
+
+			PairwiseSimilarity ps;
+			ps.Initialize(_nrImages, _sizeSet2, _useMask, _masks, _simType, _nrMasks);
+			string name = _imagedir + _files[i];
+			_images[0]->Read(name.c_str());
+
+
+			ps.LoadImages(_images);
+			ps.GetSimilarities();
+			double currentSim = ps.GetSimilarity(0,0,0);
+			_results_nmi[i] = currentSim;
+			delete _images[0];
+			delete _images[1];
+			delete [] _images;
+
+		}
+	}
+
+ };
+
+#endif
+
+
+void usage()
+{
+	cerr << "usage LEAPsimilarity [atlasNames] [atlasDir] [targetImage] [ROI] [outFile]" << endl;
+
+		
+}
+int main(int argc, char **argv)
+{
+	if(argc < 6){
+		usage();
+		exit(1);
+	}
+
+	string imagefile = argv[1];
+	argc--;
+	argv++;
+	string imagedir = argv[1];
+	argc--;
+	argv++;
+	string targetImage = argv[1];
+	argc--;
+	argv++;
+	string maskName = argv[1];
+	argc--;
+	argv++;
+	string outname = argv[1];
+	argc--;
+	argv++;
+
+	bool ok;
+	while (argc > 1){
+		ok = false;
+		if (ok == false){
+			cerr << "Can not parse argument " << argv[1] << endl;
+			usage();
+		}
+	}
+	//read imagenames
+	vector<string> files = vector<string>();
+	string line;
+	ifstream from(imagefile.c_str());
+	if (!from) {
+		cerr << "Can't open file "
+		<< endl;
+		exit(1);
+	}
+
+	if (from.is_open())	{
+		while (! from.eof() ) {
+		  getline (from,line);
+		  if(line != ""){
+		  	string fileName = imagedir+ line;
+		  	files.push_back(line);
+		  }
+		}
+	from.close();
+	}
+
+	//nrImages gives the number of atlas images excluding the target.
+//	int nrImages = files.size();
+//	files.push_back(targetImage);
+
+//	cout << "Nr of regions: " << endl;
+
+
+	irtkGreyImage ** masks;
+	masks = new irtkGreyImage*[1];
+	masks[0] = new irtkGreyImage();
+	masks[0]->Read(maskName.c_str());
+	irtkGreyPixel *ptr = masks[0]->GetPointerToVoxels();
+	for(int i = 0; i < masks[0]->GetNumberOfVoxels(); i++){
+		*ptr = *ptr - 1;
+		ptr++;
+	}
+
+	double results[files.size()];
+	irtkGreyImage * target;
+	target = new irtkGreyImage();
+	target->Read(targetImage.c_str());
+
+	#ifdef HAS_TBB
+	task_scheduler_init init;
+	MultiThreadedLeapSimilarity evaluate(imagedir, masks, results, target, files);
+	int blocks = 2;
+	parallel_for(blocked_range<int>(0, int(files.size()), int(blocks)), evaluate);
+
+	#else
+
+	bool useMask = true; //always use a mask
+	int simType = 1; //NMI
+	int sizeSet2 = 1; // we only compare one image to all the others
+	int nrMasks = 1; // always use one mask
+	int nrImages = 1; // one image after the other
+
+
+
+	irtkGreyImage ** images;
+	images = new irtkGreyImage*[2]; // two images: current atlas and target image
+	images[0] = new irtkGreyImage(); // first image will be the current atlas
+	images[1] = new irtkGreyImage(); // second image will always be the target
+	for(int i = 0; i < int(files.size()); i++){
+
+
+		       *images[1] = *target;
+
+		cout << "Atlas " << i << ". Image ";
+
+		PairwiseSimilarity ps;
+		ps.Initialize(nrImages, sizeSet2, useMask, masks, simType, nrMasks);
+		string name = imagedir + files[i];
+		images[0]->Read(name.c_str());
+		ps.LoadImages(images);
+		ps.GetSimilarities();
+		double currentSim = ps.GetSimilarity(0,0,0);
+		results[i] = currentSim;
+	}
+
+	#endif
+
+	cout << "Writing " << outname << endl;
+	ofstream output;
+	ostringstream stm;
+	output.open(outname.c_str());
+	for(int i = 0; i < files.size(); i++){
+		output << results[i] << endl;
+	}
+	output.close();
+
+
+
+
+}
