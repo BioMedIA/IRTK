438f9fe2a767a4856f8dc21df749200f807df39d
Robin Wolz <r.wolz@imperial.ac.uk>
2012-07-19 10:27:04 +0000

    Added option to normalize with a piecewise linear function as proposed by Nyul et al.

--- packages/applications/normalize.cc
+++ packages/applications/normalize.cc
@@ -14,6 +14,7 @@
 #include <irtkHistogram.h>
 #include <irtkSegmentationFunction.h>
 #include <irtkGaussianBlurring.h>
+#include <irtkNormalizeNyul.h>
 
 // Default filenames
 char *target_name = NULL, *source_name = NULL, *sourceoutput_name = NULL, *targetoutput_name = NULL;
@@ -26,6 +27,7 @@ void usage()
   cerr << "<-Tp value>        Target padding value" << endl;
   cerr << "<-Sp value>        Source padding value" << endl;
   cerr << "<-equalize paddingvalue targetoutput>  equalize the image's histogram and backproject" << endl;
+  cerr << "<-piecewise>       use a piecewise linear function as suggested by Nyul et al." << endl;
   exit(1);
 }
 
@@ -34,6 +36,7 @@ int main(int argc, char **argv)
   int i, j, k, n, ok;
   int target_padding, source_padding;
   double a, b, cov, var, x_avg, y_avg,x,y,z;
+  bool piecewise = false;
 
   if(argc < 4) usage();
 
@@ -106,43 +109,23 @@ int main(int argc, char **argv)
 
 		ok = true;
 	}
+    if ((ok == false) && (strcmp(argv[1], "-piecewise") == 0)) {
+      argc--;
+      argv++;
+      piecewise = true;
+      ok = true;
+    }
     if (ok == false) {
       cerr << "Can not parse argument " << argv[1] << endl;
       usage();
     }
   }
-
-  n = 0;
-  x_avg = 0;
-  y_avg = 0;
-  for (k = 0; k < source.GetZ(); k++) {
-    for (j = 0; j < source.GetY(); j++) {
-      for (i = 0; i < source.GetX(); i++) {
-		  x = i; y = j; z = k;
-		  source.ImageToWorld(x,y,z);
-		  target.WorldToImage(x,y,z);
-		  x = round(x); y = round(y); z = round(z);
-		  if(x >= 0 && x < target.GetX()
-			  && y >= 0 && y < target.GetY()
-			  && z >= 0 && z < target.GetZ()){
-				  if ((source(i, j, k) > source_padding) && (target(x,y,z) > target_padding)) {
-                      n++;
-                      x_avg += source(i, j, k);
-                      y_avg += target(x, y, z);
-				  }
-		  }
-      }
-    }
-  }
-  if (n == 0) {
-    cerr << "Normalize: Number of samples should be larger than zero" << endl;
-    exit(1);
-  }
-
-  cov = 0;
-  var = 0;
-  for (k = 0; k < source.GetZ(); k++) {
-	  for (j = 0; j < source.GetY(); j++) {
+  if(piecewise == false){
+	  n = 0;
+	  x_avg = 0;
+	  y_avg = 0;
+	  for (k = 0; k < source.GetZ(); k++) {
+		for (j = 0; j < source.GetY(); j++) {
 		  for (i = 0; i < source.GetX(); i++) {
 			  x = i; y = j; z = k;
 			  source.ImageToWorld(x,y,z);
@@ -151,30 +134,63 @@ int main(int argc, char **argv)
 			  if(x >= 0 && x < target.GetX()
 				  && y >= 0 && y < target.GetY()
 				  && z >= 0 && z < target.GetZ()){
-					  if ((source(i, j, k) > source_padding) && (target(x, y, z) > target_padding)) {
-						  cov += (source(i, j, k) - x_avg) * (target(x, y, z) - y_avg);
-						  var += (source(i, j, k) - x_avg) * (source(i, j, k) - x_avg);
+					  if ((source(i, j, k) > source_padding) && (target(x,y,z) > target_padding)) {
+						  n++;
+						  x_avg += source(i, j, k);
+						  y_avg += target(x, y, z);
 					  }
 			  }
 		  }
+		}
+	  }
+	  if (n == 0) {
+		cerr << "Normalize: Number of samples should be larger than zero" << endl;
+		exit(1);
+	  }
+
+	  cov = 0;
+	  var = 0;
+	  for (k = 0; k < source.GetZ(); k++) {
+		  for (j = 0; j < source.GetY(); j++) {
+			  for (i = 0; i < source.GetX(); i++) {
+				  x = i; y = j; z = k;
+				  source.ImageToWorld(x,y,z);
+				  target.WorldToImage(x,y,z);
+				  x = round(x); y = round(y); z = round(z);
+				  if(x >= 0 && x < target.GetX()
+					  && y >= 0 && y < target.GetY()
+					  && z >= 0 && z < target.GetZ()){
+						  if ((source(i, j, k) > source_padding) && (target(x, y, z) > target_padding)) {
+							  cov += (source(i, j, k) - x_avg) * (target(x, y, z) - y_avg);
+							  var += (source(i, j, k) - x_avg) * (source(i, j, k) - x_avg);
+						  }
+				  }
+			  }
+		  }
+	  }
+	  cov /= n;
+	  var /= n;
+	  b = cov / var;
+	  a = y_avg - b * x_avg;
+
+	  cout << "Scaling = " << b << endl;
+	  cout << "Offset  = " << a << endl;
+
+	  for (k = 0; k < source.GetZ(); k++) {
+		for (j = 0; j < source.GetY(); j++) {
+		  for (i = 0; i < source.GetX(); i++) {
+			if (source(i, j, k) > source_padding) {
+				source(i, j, k) = round(a + b * source(i, j, k));
+			}
+		  }
+		}
 	  }
   }
-  cov /= n;
-  var /= n;
-  b = cov / var;
-  a = y_avg - b * x_avg;
-
-  cout << "Scaling = " << b << endl;
-  cout << "Offset  = " << a << endl;
-
-  for (k = 0; k < source.GetZ(); k++) {
-    for (j = 0; j < source.GetY(); j++) {
-      for (i = 0; i < source.GetX(); i++) {
-        if (source(i, j, k) > source_padding) {
-            source(i, j, k) = round(a + b * source(i, j, k));
-        }
-      }
-    }
+  else{ //if piecewise == true -> use piecewise linear approach proposed by Nyul et al.
+	  NormalizeNyul nn(source, target);
+	  nn.SetPadding(source_padding, target_padding);
+	  nn.Run();
+	  source = nn.GetOutput();
   }
 
   // Write image
