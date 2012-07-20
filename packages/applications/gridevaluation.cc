585a9ae2d1382f1ef02f2968aeb7315eb2f2edf1
Wenzhe Shi <wenzhe.shi07@imperial.ac.uk>
2012-07-20 09:30:52 +0000

    unstructured grid evaluation

---
+++ packages/applications/gridevaluation.cc
@@ -0,0 +1,136 @@
+/*=========================================================================
+
+  Library   : Image Registration Toolkit (IRTK)
+  Module    : $Id: pevaluation.cc 599 2012-05-01 13:00:42Z ws207 $
+  Copyright : Imperial College, Department of Computing
+              Visual Information Processing (VIP), 2008 onwards
+  Date      : $Date: 2012-05-01 14:00:42 +0100 (Tue, 01 May 2012) $
+  Version   : $Revision: 599 $
+  Changes   : $Author: ws207 $
+
+=========================================================================*/
+
+#ifdef HAS_VTK
+
+#include <vtkUnstructuredGrid.h>
+#include <vtkUnstructuredGridReader.h>
+#include <vtkUnstructuredGridWriter.h>
+
+#include <irtkTransformation.h>
+
+char *_target_name = NULL, *_source_name = NULL;
+char *resultout_name = NULL;
+
+void usage()
+{
+  cerr << "Usage: pevaluation [target] [source]" << endl;
+  cerr << "<-output file>       Result output file" << endl;
+  exit(1);
+}
+
+int main(int argc, char **argv)
+{
+  int i, ok;
+  double error, source_point[3], target_point[3];
+
+  if (argc < 3) {
+    usage();
+  }
+
+  // Parse filenames
+  _target_name = argv[1];
+  argv++;
+  argc--;
+  _source_name = argv[1];
+  argv++;
+  argc--;
+
+  // Parse remaining parameters
+  while (argc > 1){
+    ok = false;
+	if ((ok == false) && (strcmp(argv[1], "-output") == 0)){
+      argc--;
+      argv++;
+      resultout_name = argv[1];
+      argc--;
+      argv++;
+      ok = true;
+    }
+    if (ok == false){
+      cerr << "Can not parse argument " << argv[1] << endl;
+      usage();
+    }
+  } 
+
+  // Target pipeline
+  cout << "Reading target ... " << _target_name << endl;
+  vtkUnstructuredGridReader *target_reader = vtkUnstructuredGridReader::New();
+  target_reader->SetFileName(_target_name);
+  target_reader->Modified();
+  target_reader->Update();
+  vtkUnstructuredGrid *target = vtkUnstructuredGrid::New();
+  target = target_reader->GetOutput();
+  target->Update();
+
+  // Source pipeline
+  cout << "Reading source ... " << _source_name << endl;
+  vtkUnstructuredGridReader *source_reader = vtkUnstructuredGridReader::New();
+  source_reader->SetFileName(_source_name);
+  source_reader->Modified();
+  source_reader->Update();
+  vtkUnstructuredGrid *source = vtkUnstructuredGrid::New();
+  source = source_reader->GetOutput();
+  source->Update();
+
+  if (target->GetNumberOfPoints() != source->GetNumberOfPoints()) {
+    cerr << "Landmarks should have equal number of points" << endl;
+    exit(1);
+  }
+
+  error = 0;
+  for (i = 0; i < target->GetNumberOfPoints(); i++) {
+    target->GetPoints()->GetPoint (i, target_point);
+    source->GetPoints()->GetPoint (i, source_point);
+    error += sqrt((target_point[0] - source_point[0]) * (target_point[0] - source_point[0]) +
+                  (target_point[1] - source_point[1]) * (target_point[1] - source_point[1]) +
+                  (target_point[2] - source_point[2]) * (target_point[2] - source_point[2]));
+    cout << "Error for landmark " << i + 1 << " = " <<
+         sqrt((target_point[0] - source_point[0]) * (target_point[0] - source_point[0]) +
+              (target_point[1] - source_point[1]) * (target_point[1] - source_point[1]) +
+              (target_point[2] - source_point[2]) * (target_point[2] - source_point[2])) << endl;
+
+  }
+
+  error /= (double)target->GetNumberOfPoints();
+  cout << "RMS = " << error << " mm" << endl;
+
+  double tstd = 0, stderror = 0;
+  for (i = 0; i < target->GetNumberOfPoints(); i++){
+	  target->GetPoints()->GetPoint (i, target_point);
+	  source->GetPoints()->GetPoint (i, source_point);
+	  tstd = sqrt((target_point[0] - source_point[0]) * (target_point[0] - source_point[0]) +
+		  (target_point[1] - source_point[1]) * (target_point[1] - source_point[1]) +
+		  (target_point[2] - source_point[2]) * (target_point[2] - source_point[2]));
+	  tstd = pow(tstd - error,2);
+	  stderror += tstd;
+  }
+  stderror=sqrt(stderror/double(target->GetNumberOfPoints()));
+  cout<<"standard deviation is " << stderror  << " mm" << endl;
+
+  if(resultout_name){
+	  cerr << "Writing Results: " << resultout_name << endl;
+	  ofstream fout(resultout_name,ios::app);	  
+	  fout << error << " " << stderror << endl;
+	  fout.close();
+  }
+}
+
+#else
+
+#include <irtkImage.h>
+
+int main( int argc, char *argv[] )
+{
+  cerr << argv[0] << " needs to be compiled with the VTK library " << endl;
+}
+#endif
---
+++ packages/applications/gridtransformation.cc
@@ -0,0 +1,164 @@
+/*=========================================================================
+
+  Library   : Image Registration Toolkit (IRTK)
+  Module    : $Id: ptransformation.cc 339 2011-06-06 21:54:45Z ws207 $
+  Copyright : Imperial College, Department of Computing
+              Visual Information Processing (VIP), 2008 onwards
+  Date      : $Date: 2011-06-06 22:54:45 +0100 (Mon, 06 Jun 2011) $
+  Version   : $Revision: 339 $
+  Changes   : $Author: ws207 $
+
+=========================================================================*/
+
+#include <irtkImage.h>
+
+#include <irtkTransformation.h>
+
+#ifdef HAS_VTK
+
+#include <vtkUnstructuredGridWriter.h>
+#include <vtkUnstructuredGridReader.h>
+#include <vtkPoints.h>
+#include <vtkUnstructuredGrid.h>
+
+// Default filenames
+char *input_name = NULL, *output_name = NULL;
+char *dof_name  = NULL;
+
+void usage()
+{
+  cerr << "Usage: gridtransformation [input] [output] <options>\n"
+       << endl;
+  cerr << "<-dofin file>      Transformation" << endl;
+  cerr << "<-invert>          Invert transformation" << endl;
+  cerr << "<-source image>    reference source image" << endl;
+  cerr << "<-target image>    reference target image" << endl;
+  cerr << "<-partial number>  Transform 0-number points copy rest"<<endl;
+  exit(1);
+}
+
+int main(int argc, char **argv)
+{
+  int ok, invert,sourceon,targeton, pnumber;
+  irtkPointSet output, input;
+  irtkTransformation *transformation;
+  irtkGreyImage target,source;
+
+  // Check command line
+  if (argc < 3) {
+    usage();
+  }
+  pnumber = 0;
+
+
+  // Parse input and output point lists
+  input_name = argv[1];
+  argc--;
+  argv++;
+  output_name = argv[1];
+  argc--;
+  argv++;
+
+  // Default parameters
+  invert = false;
+  targeton = false;
+  sourceon = false;
+
+  // Parse arguments
+  while (argc > 1) {
+    ok = false;
+    if ((ok == false) && (strcmp(argv[1], "-dofin") == 0)) {
+      argc--;
+      argv++;
+      dof_name = argv[1];
+      argc--;
+      argv++;
+      ok = true;
+    }
+    if ((ok == false) && (strcmp(argv[1], "-invert") == 0)) {
+      argc--;
+      argv++;
+      invert = true;
+      ok = true;
+    }
+	if ((ok == false) && (strcmp(argv[1], "-source") == 0)) {
+      argc--;
+      argv++;
+      ok = true;
+	  sourceon = true;
+	  source.Read(argv[1]);
+	  argc--;
+      argv++;
+    }
+	if ((ok == false) && (strcmp(argv[1], "-target") == 0)) {
+		argc--;
+		argv++;
+		targeton = true;
+		target.Read(argv[1]);
+		ok = true;
+		argc--;
+		argv++;
+    }
+	if ((ok == false) && (strcmp(argv[1], "-partial") == 0)) {
+		argc--;
+		argv++;
+		pnumber = atoi(argv[1]);
+		ok = true;
+		argc--;
+		argv++;
+    }
+    if (ok == false) {
+      cerr << "Can not parse argument " << argv[1] << endl;
+      usage();
+    }
+  }
+
+  if (dof_name != NULL) {
+    // Read transformation
+    transformation = irtkTransformation::New(dof_name);
+  } else {
+    // Create identity transformation
+    transformation = new irtkRigidTransformation;
+  }
+
+  // Read point lists
+  vtkUnstructuredGridReader *reader = vtkUnstructuredGridReader::New();
+  reader->SetFileName(input_name);
+  reader->Update();
+
+  vtkUnstructuredGrid* surface = reader->GetOutput();
+  vtkPoints*   points  = surface->GetPoints();
+
+  for (int i=0; i < points->GetNumberOfPoints(); i++) {
+    double p[3];
+    points->GetPoint(i,p);
+	if(sourceon&&targeton){
+		target.WorldToImage(p[0],p[1],p[2]);
+		//p[1] = target.GetY() - p[1];
+		source.ImageToWorld(p[0],p[1],p[2]);
+	}
+	if(i<pnumber || pnumber == 0){
+		if (invert == false) {
+			transformation->Transform(p[0],p[1],p[2]);
+		} else {
+			transformation->Inverse(p[0],p[1],p[2]);
+		}
+	}
+    points->SetPoint(i,p);
+  }
+
+  // Write the final set
+  vtkUnstructuredGridWriter   *writer = vtkUnstructuredGridWriter::New();
+  writer->SetFileName(output_name);
+  writer->SetInput(surface);
+  writer->Write();
+}
+
+#else
+#include <irtkCommon.h>
+int main(int argc, char **argv)
+{
+  cerr << "ptransformation: this program needs to be compiled with vtk enabled.\n";
+  return 0;
+}
+#endif
