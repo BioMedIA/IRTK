330b0cde351139fd43a67763074a810d7a8c32ff
Daniel Rueckert <d.rueckert@imperial.ac.uk>
2013-02-11 16:39:43 +0000

    Application threshold now always produces the same output type as is the input

--- applications/threshold.cc
+++ applications/threshold.cc
@@ -1,51 +1,90 @@
 /*=========================================================================
 
-  Library   : Image Registration Toolkit (IRTK)
-  Module    : $Id$
-  Copyright : Imperial College, Department of Computing
-              Visual Information Processing (VIP), 2008 onwards
-  Date      : $Date$
-  Version   : $Revision$
-  Changes   : $Author$
+ Library   : Image Registration Toolkit (IRTK)
+ Module    : $Id$
+ Copyright : Imperial College, Department of Computing
+ Visual Information Processing (VIP), 2008 onwards
+ Date      : $Date$
+ Version   : $Revision$
+ Changes   : $Author$
 
-=========================================================================*/
+ =========================================================================*/
 
 #include <irtkImage.h>
 
 void usage()
 {
-  cerr << "Usage: threshold [input] [output] [threshold]" << endl;
-  exit(1);
+	cerr << "Usage: threshold [input] [output] [threshold]" << endl;
+	exit(1);
+}
+
+template<typename VoxelType> void threshold(irtkGenericImage<VoxelType> &image, double t)
+{
+	int x, y, z;
+
+	for (z = 0; z < image.GetZ(); z++) {
+		for (y = 0; y < image.GetY(); y++) {
+			for (x = 0; x < image.GetX(); x++) {
+				if (image(x, y, z) <= t) {
+					image(x, y, z) = 0;
+				} else {
+					image(x, y, z) = 1;
+				}
+			}
+		}
+	}
+
 }
 
 int main(int argc, char **argv)
 {
-  int x, y, z;
-  irtkGreyPixel threshold;
+	double t;
 
-  if (argc != 4) {
-    usage();
-  }
+	if (argc != 4) {
+		usage();
+	}
 
-  // Read image
-  irtkGreyImage image(argv[1]);
+	// Read image
+	irtkImage *image = irtkImage::New(argv[1]);
 
-  // Threshold value
-  threshold = atoi(argv[3]);
+	// Read threshold value
+	t = atof(argv[3]);
 
-  for (z = 0; z < image.GetZ(); z++) {
-    for (y = 0; y < image.GetY(); y++) {
-      for (x = 0; x < image.GetX(); x++) {
-        if (image(x, y, z) <= threshold) {
-          image(x, y, z) = 0;
-        } else {
-          image(x, y, z) = 1;
-        }
-      }
-    }
-  }
+	// Apply threshold
+	switch (image->GetScalarType()) {
+		case IRTK_VOXEL_CHAR: {
+			threshold(*(dynamic_cast<irtkGenericImage<char> *>(image)), t);
+			break;
+		}
+		case IRTK_VOXEL_UNSIGNED_CHAR: {
+			threshold(*(dynamic_cast<irtkGenericImage<unsigned char> *>(image)), t);
+			break;
+		}
+		case IRTK_VOXEL_SHORT: {
+			threshold(*(dynamic_cast<irtkGenericImage<short> *>(image)), t);
+			break;
+		}
+		case IRTK_VOXEL_UNSIGNED_SHORT: {
+			threshold(*(dynamic_cast<irtkGenericImage<unsigned short> *>(image)), t);
+			break;
+		}
+		case IRTK_VOXEL_FLOAT: {
+			threshold(*(dynamic_cast<irtkGenericImage<float> *>(image)), t);
+			break;
+		}
+		case IRTK_VOXEL_DOUBLE: {
+			threshold(*(dynamic_cast<irtkGenericImage<double> *>(image)), t);
+			break;
+		}
+		default:
+			cerr << "Voxel type not supported" << endl;
+			exit(1);
+	}
 
-  image.Write(argv[2]);
+	//threshold(image, t);
 
-  return 0;
+	// Write image
+	image->Write(argv[2]);
+
+	return 0;
 }
