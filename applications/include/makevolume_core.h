f2872815aa57a1024f97681c50d5be8d99bf019e
Ira Ktena <ira.ktena12@imperial.ac.uk>
2014-04-23 10:33:04 +0100

    Replaced indexx in makevolume application

--- applications/include/makevolume_core.h
+++ applications/include/makevolume_core.h
@@ -1,6 +1,17 @@
 #ifndef makevolume_core_h
 #define makevolume_core_h
 
-long unsigned* indexing(unsigned long n, float* array);
+// functor for indexing
+template<class T>
+struct index_sorting {
+  const T _values;
+
+public:
+  index_sorting(const T values) : _values(values) {};
+  bool operator() (long unsigned a, long unsigned b) const {
+    return _values[a] < _values[b]; }    
+};
+
+long unsigned* indexing(long unsigned n, float* array);
 
 #endif // makevolume_core_h
--- applications/src/makevolume_core.cc
+++ applications/src/makevolume_core.cc
@@ -1,12 +1,25 @@
 #include "../include/makevolume_core.h"
 
 #include <irtkImage.h>
-#include <nr.h>
 
-long unsigned* indexing(unsigned long n, float* array) {
-  long unsigned* index = new unsigned long[n];
+#include <algorithm>
 
-  indexx(n, array-1, index-1);
+long unsigned* indexing(long unsigned n, float* array) {
+  // initialize vector with the array values
+  // pointers can be used like any other iterators
+  vector<float> vec(array, array+n);
+
+  // initialize indices vector
+  vector<long unsigned> idx;
+  idx.resize(n);
+  for (long unsigned i = 0; i < n; i++) idx[i] = i;
+
+  // do the sorting
+  sort(idx.begin(), idx.end(), index_sorting<vector<float> &>(vec));
+
+  // fill the array to maintain function signature
+  long unsigned* index = new long unsigned[n];
+  for (long unsigned i = 0; i < n; i++) index[i] = idx[i]+1; 
 
   return index;
 }
