cf7ed4bc0a18c533b6ce3e4bd6307c26934a4329
Ira Ktena <ira.ktena12@imperial.ac.uk>
2014-06-23 17:23:05 +0100

    Removed recipes gasdev function from irtkRicianNoise.cc

--- image++/src/irtkRicianNoise.cc
+++ image++/src/irtkRicianNoise.cc
@@ -15,8 +15,8 @@
 #include <irtkNoise.h>
 
 // recipe includes
-#include <nr.h>
-#include <nrutil.h>
+#include <boost/random.hpp>
+#include <boost/random/normal_distribution.hpp>
 
 template <class VoxelType> irtkRicianNoise<VoxelType>::irtkRicianNoise(double Amplitude) : irtkNoise<VoxelType>(Amplitude)
 {
@@ -29,26 +29,32 @@ template <class VoxelType> const char *irtkRicianNoise<VoxelType>::NameOfClass()
 
 template <class VoxelType> double irtkRicianNoise<VoxelType>::Run(int x, int y, int z, int t)
 {
-#ifdef OLD_RICIAN
-  // For bug-ward consistency:
+  boost::mt19937 rng;
+  rng.seed(this->_Init);
 
+  boost::normal_distribution<> nd(0.0, 1.0);
+  boost::variate_generator<boost::mt19937&,
+			   boost::normal_distribution<> > var_nor(rng, nd);
+
+#ifdef OLD_RICIAN
+  // For backward consistency:
   if (this->_input->Get(x, y, z) <= 3*this->_Amplitude) {
     // Rayleigh distributed noise in background and air
-    double s1 = gasdev(&this->_Init);
-    double s2 = gasdev(&this->_Init);
+    double s1 = var_nor();
+    double s2 = var_nor();
     double M  = sqrt(s1*s1+s2*s2);
     return double(this->_input->Get(x, y, z, y)) + this->_Amplitude * M;
   } else {
     // Gaussian distributed noise in foreground
-    return double(this->_input->Get(x, y, z, y)) + this->_Amplitude * gasdev(&this->_Init);
+    return double(this->_input->Get(x, y, z, y)) + this->_Amplitude * var_nor();
   }
 
 #else
   // Add Rician noise by treating the magnitude image as the real part (r) of a new complex variable (r,i),
   // adding Gaussian noise to the separate parts of this complex variable, before taking the magnitude again.
   // Thanks to Ged Ridgway <gerard.ridgway@ucl.ac.uk> for this contribution.
-  double r = double(this->_input->Get(x, y, z, t)) + this->_Amplitude * gasdev(&this->_Init);
-  double i = this->_Amplitude * gasdev(&this->_Init);
+  double r = double(this->_input->Get(x, y, z, t)) + this->_Amplitude * var_nor();
+  double i = this->_Amplitude * var_nor();
   return sqrt(r*r + i*i);
 
 #endif
