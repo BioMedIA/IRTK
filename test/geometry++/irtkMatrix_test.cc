0534f3c8434c8e5ed781f4bccd9638a372b9c57e
Ira Ktena <ira.ktena12@imperial.ac.uk>
2014-07-10 15:48:44 +0100

    Unit tests for irtkMatrix.Eigenvalues

--- test/geometry++/irtkMatrix_test.cc
+++ test/geometry++/irtkMatrix_test.cc
@@ -153,3 +153,84 @@ TEST(Geometry_irtkMatrix, LeastSquaresFit) {
       ASSERT_NEAR(expected_x[i], x(i), EPSILON);
    }
 }
+
+TEST(Geometry_irtkMatrix, Eigenvalues_non_symmetric_matrix) {
+   irtkMatrix matrix(3, 3);
+   matrix.Put(0, 0, 67);
+   matrix.Put(0, 1, 120);
+   matrix.Put(0, 2, 170);
+   matrix.Put(1, 0, -24);
+   matrix.Put(1, 1, -35);
+   matrix.Put(1, 2, -40);
+   matrix.Put(2, 0, -8);
+   matrix.Put(2, 1, -20);
+   matrix.Put(2, 2, -35);
+
+   irtkMatrix E1, E2;
+   irtkVector e;
+
+   matrix.Eigenvalues(E1, e, E2);
+    
+   irtkVector exp(3);
+   exp(0) = 52768.81418;
+   exp(1) = 110.18485;
+   exp(2) = 0.00096;
+
+   for (int i = 0; i < e.Rows(); i++) {
+      ASSERT_NEAR(exp(i), e(i), EPSILON);
+   }
+}
+
+TEST(Geometry_irtkMatrix, Eigenvalues_symmetric_matrix) {
+   irtkMatrix matrix(3, 3);
+   matrix.Put(0, 0, 1);
+   matrix.Put(0, 1, 7);
+   matrix.Put(0, 2, 3);
+   matrix.Put(1, 0, 7);
+   matrix.Put(1, 1, 4);
+   matrix.Put(1, 2, -5);
+   matrix.Put(2, 0, 3);
+   matrix.Put(2, 1, -5);
+   matrix.Put(2, 2, 6);
+
+   irtkMatrix E1, E2;
+   irtkVector e;
+
+   matrix.Eigenvalues(E1, e, E2);
+    
+   irtkVector exp(3);
+   exp(0) = -7.007928;
+   exp(1) = 7.03595;
+   exp(2) = 10.971982;
+
+   for (int i = 0; i < e.Rows(); i++) {
+      ASSERT_NEAR(exp(i), e(i), EPSILON);
+   }
+}
+
+TEST(Geometry_irtkMatrix, Eigenvalues_diag_matrix) {
+   irtkMatrix matrix(3, 3);
+   matrix.Put(0, 0, 1);
+   matrix.Put(0, 1, 1);
+   matrix.Put(0, 2, 0);
+   matrix.Put(1, 0, 0);
+   matrix.Put(1, 1, 1);
+   matrix.Put(1, 2, 1);
+   matrix.Put(2, 0, 1);
+   matrix.Put(2, 1, 0);
+   matrix.Put(2, 2, 1);
+
+   irtkMatrix E1, E2;
+   irtkVector e;
+
+   matrix.Eigenvalues(E1, e, E2);
+    
+   irtkVector exp(3);
+   exp(0) = 2;
+   exp(1) = 0;
+   exp(2) = 1;
+
+   for (int i = 0; i < e.Rows(); i++) {
+      ASSERT_NEAR(exp(i), e(i), EPSILON);
+   }
+}
